cb05f29203adf55424ff46e982c756b22e6d06f8
bogdddy <70880882+bogdddy@users.noreply.github.com>
2021-07-29 18:50:58 +0200

    Update main.js

--- main.js
+++ main.js
@@ -1,1 +1,7 @@
-const myhref = link =>  window.location.href = link
+$("#linkedin").click(function () {
+  window.location.href = 'https://www.linkedin.com/in/bogdan-struk-33265b1a3/'
+})
+
+$("#github").click(function () {
+  window.location.href = 'https://github.com/bogdddy'
+})
