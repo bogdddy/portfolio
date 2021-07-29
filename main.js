71cbf8e31927015d977d38c511a23b785450a80f
BogDaddy <70880882+BogDaddy@users.noreply.github.com>
2021-07-29 19:35:36 +0200

    Update social buttons

--- main.js
+++
@@ -1,7 +0,0 @@
-$("#linkedin").click(function () {
-  window.location.href = 'https://www.linkedin.com/in/bogdan-struk-33265b1a3/'
-})
-
-$("#github").click(function () {
-  window.location.href = 'https://github.com/bogdddy'
-})
